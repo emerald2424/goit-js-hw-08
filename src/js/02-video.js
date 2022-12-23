f36031cd95e0b205ca42354439d3ac99d9b7433c
emerald2424 <109916178+emerald2424@users.noreply.github.com>
2022-12-23 21:02:03 +0200

    solves the tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,27 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('timeupdate', throttle((function(currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+}), 1000));
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+
+const SECONDS = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(SECONDS).then(function(seconds) {
+    // seconds = localStorage.getItem("videoplayer-current-time");
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+onReload();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+    const data = {};
+    data.email = input.value;
+    data.message = textarea.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+    console.log(data);
+}
+
+function onReload() {
+    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedData) {
+        input.value = savedData.email;
+        textarea.value = savedData.message;
+    } 
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault;
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
